9745433833a07da4f22f5a41f16389d68847558a
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-21 21:30:11 -0400

    Add complex object into initialState to make things crazy
    Things getting more crazy at youtubeVideo property in state. You can see our reducer is getting more "crazy".
    
    That is why combineReducers exist to handle each reducer separately. Each reducer will handle each state separately.
    
    Then, we combine all reducers together to achieve the same effect... but with cleaner code!

--- index.js
+++ index.js
@@ -26,9 +26,30 @@ function removeFavoriteThing(thing) {
   }
 }
 
+function setYoutubeTitle(title) {
+  return {
+    type: "SET_YOUTUBE_TITLE",
+    payload: title
+  }
+}
+
+function upVote() {
+  return {
+    type: "UP_VOTE"
+  }
+}
+
 const initialState = {
   count: 0,
-  favoriteThings: []
+  favoriteThings: [],
+  youtubeVideo: {
+    title: "",
+    viewCount: 0,
+    votes: {
+      up: 0,
+      down: 0
+    }
+  }
 }
 
 //the reducer's job is to make changes to global state based on the action
@@ -50,9 +71,27 @@ function reducer(state = initialState, action) {
     case "ADD_FAVORITE_THING":
       return {...state, favoriteThings: [...state.favoriteThings, action.payload]}
     case "REMOVE_FAVORITE_THING":
+      //another way is to create a copy of the array
+      //then you can do all sort of method that mutate that copy
       return {
         ...state, 
-        favoriteThings: state.favoriteThings.filter(thing => thing !== action.payload)
+        favoriteThings: state.favoriteThings.filter(thing => thing.toLowerCase() !== action.payload.toLowerCase())
+      }
+    case "SET_YOUTUBE_TITLE": 
+      return {
+        ...state,
+        youtubeVideo: {...state.youtubeVideo, title: action.payload}
+      }
+    case "UP_VOTE":
+      return {
+        ...state, 
+        youtubeVideo: {
+          ...state.youtubeVideo, 
+          votes: {
+            ...state.youtubeVideo.votes, 
+            up: state.youtubeVideo.votes.up + 1
+          }
+        }
       }
     //need a default case, incase no argument or invalid argument passed in
     default:
@@ -87,3 +126,8 @@ store.dispatch(changeCount(-1))
 store.dispatch(addFavoriteThing("Red Rosei"))
 store.dispatch(addFavoriteThing("yes yes"))
 store.dispatch(removeFavoriteThing("yes yes"))
+
+store.dispatch(setYoutubeTitle("Learn Redux"))
+store.dispatch(upVote())
+store.dispatch(upVote())
+store.dispatch(upVote())
